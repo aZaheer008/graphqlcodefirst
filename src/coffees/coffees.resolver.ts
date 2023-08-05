ff9a59f0db81c18d3b18ce9084a25fa4f86cd934
ahmedzaheerSys <ahmed.zaheer@systemsltd.com>
2023-08-05 11:43:13 +0500

    service initiate and all function defined in service

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,10 +1,25 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { CoffeesService } from './coffees.service';
+import { Query, Resolver, Args, ID, Mutation } from '@nestjs/graphql';
 import { Coffee } from './entities/coffee.entity/coffee.entity';
+import { ParseIntPipe } from '@nestjs/common';
+import { CreateCoffeeInput } from './dto/create-coffee.input/create-coffee.input';
 
 @Resolver()
 export class CoffeesResolver {
+    constructor(private readonly CoffeesService : CoffeesService){}
+
     @Query(() => [Coffee], { name : 'coffees'})
     async findAll() {
-        return [];
+        return this.CoffeesService.findAll();
+    }
+
+    @Query(() => Coffee, { name : 'coffee', nullable : true})
+    async findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number){
+        return this.CoffeesService.findOne(id);
+    }
+
+    @Mutation(() => Coffee, { name: 'createCoffee', nullable : true })
+    async create(@Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput){
+        return this.CoffeesService.create(createCoffeeInput);
     }
 }
